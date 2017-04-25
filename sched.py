7392b6449227a835fd562299d1a1efd7f3c70425
pcuste1 <pcuste1@umbc.edu>
2017-04-24 22:55:32 -0400

    Code that 'should' connect to the

--- sched.py
+++ sched.py
@@ -8,7 +8,7 @@ class Logging():
   # Output: None
   # Description: Opens the logfile and initializes the class
   def __init__(self):
-    self.logFile = open("sched_log_"+ str(dt.time()), "w")
+    self.logFile = open("log_sched"+ str(dt.time()), "w")
 
   # Input: None
   # Output: None
@@ -22,42 +22,50 @@ class Logging():
   def log(self,logString):
     self.logFile.write(logString)
 
-def testDB():
+def testDB(log):
   try:
-    connection = mc.connect (host = "maria.db",
-                              user = "cblake2",
+    """
+    Amazon Web Services RDS Free Tier
+
+    Hostname: ezpz.c6pi6kjq4ljd.us-east-1.rds.amazonaws.com
+    Database Name: ScheduleMe
+    Username: client
+    Password: *You already know*
+    """
+    connection = mc.connect (host = "ezpz.c6pi6kjq4ljd.us-east-1.rds.amazonaws.com",
+                              user = "client",
                               passwd = "PasswOrd",
-                              db = "company")
+                              db = "ScheduleMe")
   
+  
+    sql_command = """
+    SELECT section.course_id, course.title, section.sec_id, professor.last_name, professor.first_name, section.room_num, time_slot.days, time_slot.start_hr, time_slot.start_min, time_slot.end_hr, time_slot.end_min
+    FROM course
+    INNER JOIN section
+    ON section.course_id = course.course_id
+    INNER JOIN professor
+    ON section.prof_id = professor.prof_id
+    INNER JOIN time_slot
+    ON section.time_slot_id = time_slot.time_slot_id
+    WHERE section.course_id = "CMSC 202";
+    """
+
+    cursor = connection.cursor()
+  
+    cursor.execute(sql_command)
+    result = cursor.fetchall()
+    for r in result:
+      print r
+      
+    cursor.close()
+    connection.close()
   except mc.Error as e:
-    print("Error %d: %s" % (e.args[0], e.args[1]))
+    log.log("Error %d: %s" % (e.args[0], e.args[1]))
     sys.exit(1)
-
-  sql_command = """
-  SELECT section.course_id, course.title, section.sec_id, professor.last_name, professor.first_name, section.room_num, time_slot.days, time_slot.start_hr, time_slot.start_min, time_slot.end_hr, time_slot.end_min
-FROM course
-  INNER JOIN section
-ON section.course_id = course.course_id
-  INNER JOIN professor
-ON section.prof_id = professor.prof_id
-  INNER JOIN time_slot
-ON section.time_slot_id = time_slot.time_slot_id
-WHERE section.course_id = "CMSC 202";
-  """
-
-  cursor = connection.cursor()
-  
-  cursor.execute(sql_command)
-  result = cursor.fetchall()
-  for r in result:
-    print r
-
-  cursor.close()
-  connection.close()
 
 if __name__ == "__main__":
   print("hello, world!")
   log = Logging()
   log.log("Test")
+  testDB(log)
   log.close()
-  testDB()
